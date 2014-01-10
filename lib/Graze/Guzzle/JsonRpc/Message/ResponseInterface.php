745aeb129baca03410f8f4c46b84706a5a7ef6cf
adlawson <adlawson@gmail.com>
2014-01-10 16:51:32 +0000

    Update copyright date to 2014

--- lib/Graze/Guzzle/JsonRpc/Message/ResponseInterface.php
+++ lib/Graze/Guzzle/JsonRpc/Message/ResponseInterface.php
@@ -2,7 +2,7 @@
 /*
  * This file is part of Guzzle JsonRpc
  *
- * Copyright (c) 2013 Nature Delivered Ltd. <http://graze.com>
+ * Copyright (c) 2014 Nature Delivered Ltd. <http://graze.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
