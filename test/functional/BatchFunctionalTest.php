27a31330e1906c3a827f9c93d9b3a6a8bf1a1eae
adlawson <adlawson@gmail.com>
2014-10-02 16:19:42 +0100

    Add functional test suite

---
+++ test/functional/BatchFunctionalTest.php
@@ -0,0 +1,113 @@
+<?php
+/*
+ * This file is part of Guzzle HTTP JSON-RPC
+ *
+ * Copyright (c) 2014 Nature Delivered Ltd. <http://graze.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see  http://github.com/graze/guzzle-jsonrpc/blob/master/LICENSE
+ * @link http://github.com/graze/guzzle-jsonrpc
+ */
+namespace Graze\GuzzleHttp\JsonRpc;
+
+use Graze\GuzzleHttp\JsonRpc\Test\FunctionalTestCase;
+
+class BatchFunctionalTest extends FunctionalTestCase
+{
+    public function setUp()
+    {
+        $this->client = $this->createClient();
+    }
+
+    public function testBatchRequestWithOneChild()
+    {
+        $id = 'abc';
+        $method = 'sum';
+        $params = ['foo'=>123, 'bar'=>456];
+        $request = $this->client->request($method, $params, $id);
+        $responses = $this->client->sendAll([$request]);
+
+        $this->assertEquals(ClientInterface::SPEC, $request->getRpcVersion());
+        $this->assertEquals($id, $request->getRpcId());
+        $this->assertEquals($method, $request->getRpcMethod());
+        $this->assertEquals($params, $request->getRpcParams());
+
+        $this->assertTrue(is_array($responses));
+        $this->assertEquals(ClientInterface::SPEC, $responses[0]->getRpcVersion());
+        $this->assertEquals(array_sum($params), $responses[0]->getRpcResult());
+        $this->assertEquals($id, $responses[0]->getRpcId());
+        $this->assertEquals(null, $responses[0]->getRpcErrorCode());
+        $this->assertEquals(null, $responses[0]->getRpcErrorMessage());
+    }
+
+    public function testBatchRequestWithMultipleChildren()
+    {
+        $idA = 123;
+        $idC = 'abc';
+        $idD = 'def';
+        $methodA = 'concat';
+        $methodB = 'nofify';
+        $methodC = 'sum';
+        $methodD = 'bar';
+        $paramsA = ['foo'=>'abc', 'bar'=>'def'];
+        $paramsB = ['foo'=>false];
+        $paramsC = ['foo'=>123, 'bar'=>456];
+        $paramsD = ['foo'=>123, 'bar'=>456];
+        $requestA = $this->client->request($methodA, $paramsA, $idA);
+        $requestB = $this->client->notification($methodB, $paramsB);
+        $requestC = $this->client->request($methodC, $paramsC, $idC);
+        $requestD = $this->client->request($methodD, $paramsD, $idD);
+        $responses = $this->client->sendAll([$requestA, $requestB, $requestC, $requestD]);
+
+        $this->assertEquals(ClientInterface::SPEC, $requestA->getRpcVersion());
+        $this->assertEquals($idA, $requestA->getRpcId());
+        $this->assertEquals($methodA, $requestA->getRpcMethod());
+        $this->assertEquals($paramsA, $requestA->getRpcParams());
+        $this->assertEquals(ClientInterface::SPEC, $requestB->getRpcVersion());
+        $this->assertEquals(null, $requestB->getRpcId());
+        $this->assertEquals($methodB, $requestB->getRpcMethod());
+        $this->assertEquals($paramsB, $requestB->getRpcParams());
+        $this->assertEquals(ClientInterface::SPEC, $requestC->getRpcVersion());
+        $this->assertEquals($idC, $requestC->getRpcId());
+        $this->assertEquals($methodC, $requestC->getRpcMethod());
+        $this->assertEquals($paramsC, $requestC->getRpcParams());
+        $this->assertEquals(ClientInterface::SPEC, $requestD->getRpcVersion());
+        $this->assertEquals($idD, $requestD->getRpcId());
+        $this->assertEquals($methodD, $requestD->getRpcMethod());
+        $this->assertEquals($paramsD, $requestD->getRpcParams());
+
+        $this->assertTrue(is_array($responses));
+        $this->assertEquals(3, count($responses));
+
+        foreach ($responses as $response) {
+            if ($response->getRpcId() === $idA) {
+                $responseA = $response;
+            } elseif ($response->getRpcId() === $idC) {
+                $responseC = $response;
+            } elseif ($response->getRpcId() === $idD) {
+                $responseD = $response;
+            }
+        }
+        if (!isset($responseA) || !isset($responseC) || !isset($responseD)) {
+            $this->fail('Invalid responses');
+        }
+
+        $this->assertEquals(ClientInterface::SPEC, $responseA->getRpcVersion());
+        $this->assertEquals(implode('', $paramsA), $responseA->getRpcResult());
+        $this->assertEquals($idA, $responseA->getRpcId());
+        $this->assertEquals(null, $responseA->getRpcErrorCode());
+        $this->assertEquals(null, $responseA->getRpcErrorMessage());
+        $this->assertEquals(ClientInterface::SPEC, $responseC->getRpcVersion());
+        $this->assertEquals(array_sum($paramsC), $responseC->getRpcResult());
+        $this->assertEquals($idC, $responseC->getRpcId());
+        $this->assertEquals(null, $responseC->getRpcErrorCode());
+        $this->assertEquals(null, $responseC->getRpcErrorMessage());
+        $this->assertEquals(ClientInterface::SPEC, $responseD->getRpcVersion());
+        $this->assertEquals(null, $responseD->getRpcResult());
+        $this->assertEquals($idD, $responseD->getRpcId());
+        $this->assertTrue(is_int($responseD->getRpcErrorCode()));
+        $this->assertTrue(is_string($responseD->getRpcErrorMessage()));
+    }
+}
---
+++ test/functional/RequestFunctionalTest.php
@@ -0,0 +1,122 @@
+<?php
+/*
+ * This file is part of Guzzle HTTP JSON-RPC
+ *
+ * Copyright (c) 2014 Nature Delivered Ltd. <http://graze.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see  http://github.com/graze/guzzle-jsonrpc/blob/master/LICENSE
+ * @link http://github.com/graze/guzzle-jsonrpc
+ */
+namespace Graze\GuzzleHttp\JsonRpc;
+
+use Graze\GuzzleHttp\JsonRpc\Test\FunctionalTestCase;
+
+class RequestFunctionalTest extends FunctionalTestCase
+{
+    public function setUp()
+    {
+        $this->client = $this->createClient();
+    }
+
+    public function testConcatRequest()
+    {
+        $id = 123;
+        $method = 'concat';
+        $params = ['foo'=>'abc', 'bar'=>'def'];
+        $request = $this->client->request($method, $params, $id);
+        $response = $this->client->send($request);
+
+        $this->assertEquals(ClientInterface::SPEC, $request->getRpcVersion());
+        $this->assertEquals($id, $request->getRpcId());
+        $this->assertEquals($method, $request->getRpcMethod());
+        $this->assertEquals($params, $request->getRpcParams());
+
+        $this->assertEquals(ClientInterface::SPEC, $response->getRpcVersion());
+        $this->assertEquals(implode('', $params), $response->getRpcResult());
+        $this->assertEquals($id, $response->getRpcId());
+        $this->assertEquals(null, $response->getRpcErrorCode());
+        $this->assertEquals(null, $response->getRpcErrorMessage());
+    }
+
+    public function testSumRequest()
+    {
+        $id = 'abc';
+        $method = 'sum';
+        $params = ['foo'=>123, 'bar'=>456];
+        $request = $this->client->request($method, $params, $id);
+        $response = $this->client->send($request);
+
+        $this->assertEquals(ClientInterface::SPEC, $request->getRpcVersion());
+        $this->assertEquals($id, $request->getRpcId());
+        $this->assertEquals($method, $request->getRpcMethod());
+        $this->assertEquals($params, $request->getRpcParams());
+
+        $this->assertEquals(ClientInterface::SPEC, $response->getRpcVersion());
+        $this->assertEquals(array_sum($params), $response->getRpcResult());
+        $this->assertEquals($id, $response->getRpcId());
+        $this->assertEquals(null, $response->getRpcErrorCode());
+        $this->assertEquals(null, $response->getRpcErrorMessage());
+    }
+
+    public function testFooRequest()
+    {
+        $id = 'abc';
+        $method = 'foo';
+        $request = $this->client->request($method, [], $id);
+        $response = $this->client->send($request);
+
+        $this->assertEquals(ClientInterface::SPEC, $request->getRpcVersion());
+        $this->assertEquals($id, $request->getRpcId());
+        $this->assertEquals($method, $request->getRpcMethod());
+        $this->assertEquals(null, $request->getRpcParams());
+
+        $this->assertEquals(ClientInterface::SPEC, $response->getRpcVersion());
+        $this->assertEquals('foo', $response->getRpcResult());
+        $this->assertEquals($id, $response->getRpcId());
+        $this->assertEquals(null, $response->getRpcErrorCode());
+        $this->assertEquals(null, $response->getRpcErrorMessage());
+    }
+
+    public function testFooRequestWithNoId()
+    {
+        $method = 'foo';
+        $request = $this->client->request($method);
+        $response = $this->client->send($request);
+
+        // Grab the generated ID
+        $id = $request->getRpcId();
+
+        $this->assertEquals(ClientInterface::SPEC, $request->getRpcVersion());
+        $this->assertTrue(is_string($request->getRpcId()));
+        $this->assertEquals($method, $request->getRpcMethod());
+        $this->assertEquals(null, $request->getRpcParams());
+
+        $this->assertEquals(ClientInterface::SPEC, $response->getRpcVersion());
+        $this->assertEquals('foo', $response->getRpcResult());
+        $this->assertEquals($id, $response->getRpcId());
+        $this->assertEquals(null, $response->getRpcErrorCode());
+        $this->assertEquals(null, $response->getRpcErrorMessage());
+    }
+
+    public function testBarRequest()
+    {
+        $id = 'abc';
+        $method = 'bar';
+        $request = $this->client->request($method, [], $id);
+        $response = $this->client->send($request);
+
+        $this->assertEquals(ClientInterface::SPEC, $request->getRpcVersion());
+        $this->assertEquals($id, $request->getRpcId());
+        $this->assertEquals($method, $request->getRpcMethod());
+        $this->assertEquals(null, $request->getRpcParams());
+
+        $this->assertEquals(ClientInterface::SPEC, $response->getRpcVersion());
+        $this->assertEquals(null, $response->getRpcResult());
+        $this->assertEquals($id, $response->getRpcId());
+        $this->assertTrue(is_int($response->getRpcErrorCode()));
+        $this->assertTrue(is_string($response->getRpcErrorMessage()));
+    }
+}
