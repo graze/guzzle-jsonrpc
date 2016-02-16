12116ea4f4ad2d3cc32b88ad724c2cda97ba165c
adlawson <adlawson@gmail.com>
2016-02-16 14:45:41 +0000

    Update json_decode to accept empty string in PHP7
    Previous versions of PHP allowed empty strings, but support seems to
    have been dropped in jsond.

--- src/functions.php
+++ src/functions.php
@@ -43,8 +43,8 @@ function json_decode($json, $assoc = false, $depth = 512, $options = 0)
          JSON_ERROR_CTRL_CHAR => 'JSON_ERROR_CTRL_CHAR - Unexpected control character found',
          JSON_ERROR_SYNTAX => 'JSON_ERROR_SYNTAX - Syntax error, malformed JSON',
          JSON_ERROR_UTF8 => 'JSON_ERROR_UTF8 - Malformed UTF-8 characters, possibly incorrectly encoded',
-     ];
-    $data = \json_decode($json, $assoc, $depth, $options);
+    ];
+    $data = \json_decode($json == "" ? "{}" : $json, $assoc, $depth, $options);
 
     if (JSON_ERROR_NONE !== json_last_error()) {
         $last = json_last_error();
